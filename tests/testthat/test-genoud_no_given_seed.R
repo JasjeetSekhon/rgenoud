52ccc3619619d4662b9483523045d6635d979fd0
Jasjeet Sekhon <jasjeet.sekhon@gmail.com>
2019-01-22 22:35:23 -0500

    Removing biclaw test when no random seed is set because it is too variable

--- tests/testthat/test-genoud_no_given_seed.R
+++ tests/testthat/test-genoud_no_given_seed.R
@@ -47,43 +47,44 @@ test_that("Tests the new version of genoud() where the seeds are not given", {
   expect_equal(claw1$par, 0.9995032, tolerance=1e-6)
   expect_equal(claw1$gradients, -4.34173e-08, tolerance=1e-6)
 
+#too variable without seeds being set
   # Maximize a bivariate normal mixture which looks like a claw.
-  biclaw <- function(xx) {
-    mNd2 <- function(x1, x2, mu1, mu2, sigma1, sigma2, rho)
-    {
-      z1 <- (x1 - mu1) / sigma1
-      z2 <- (x2 - mu2) / sigma2
-      w <- (1.0 / (2.0 * pi * sigma1 * sigma2 * sqrt(1 - rho * rho)))
-      w <- w * exp(-0.5 * (z1 * z1 - 2 * rho * z1 * z2 + z2 * z2) / 
-                     (1 - rho * rho))
-      return(w)
-    }
-    x1 <- xx[1] + 1
-    x2 <- xx[2] + 1
-    
-    y <- (0.5 * mNd2(x1, x2, 0.0, 0.0, 1.0, 1.0, 0.0) +
-            0.1 * (
-              mNd2(x1, x2, -1.0, -1.0, 0.1, 0.1, 0.0) +
-                mNd2(x1, x2, -0.5, -0.5, 0.1, 0.1, 0.0) +
-                mNd2(x1, x2, 0.0, 0.0, 0.1, 0.1, 0.0) +
-                mNd2(x1, x2, 0.5, 0.5, 0.1, 0.1, 0.0) +
-                mNd2(x1, x2, 1.0, 1.0, 0.1, 0.1, 0.0)
-            ))
-    
-    return(y)
-  }
-  biclaw1 <-
-    genoud(
-      biclaw,
-      default.domains = 20,
-      nvars = 2,
-      pop.size = 5000,
-      max = TRUE,
-      print.level = 0
-    )
-  expect_equal(biclaw1$value, 1.65353, tolerance=1e-6)
-  expect_equal(biclaw1$par, c(-0.5001947, -0.5001947), tolerance=1e-6)
-  expect_equal(biclaw1$gradients, c(-1.526799e-06, -8.571643e-07), 
-               tolerance=1e-6)
+#  biclaw <- function(xx) {
+#    mNd2 <- function(x1, x2, mu1, mu2, sigma1, sigma2, rho)
+#    {
+#      z1 <- (x1 - mu1) / sigma1
+#      z2 <- (x2 - mu2) / sigma2
+#      w <- (1.0 / (2.0 * pi * sigma1 * sigma2 * sqrt(1 - rho * rho)))
+#      w <- w * exp(-0.5 * (z1 * z1 - 2 * rho * z1 * z2 + z2 * z2) / 
+#                     (1 - rho * rho))
+#      return(w)
+#    }
+#    x1 <- xx[1] + 1
+#    x2 <- xx[2] + 1
+#    
+#    y <- (0.5 * mNd2(x1, x2, 0.0, 0.0, 1.0, 1.0, 0.0) +
+#            0.1 * (
+#              mNd2(x1, x2, -1.0, -1.0, 0.1, 0.1, 0.0) +
+#                mNd2(x1, x2, -0.5, -0.5, 0.1, 0.1, 0.0) +
+#                mNd2(x1, x2, 0.0, 0.0, 0.1, 0.1, 0.0) +
+#                mNd2(x1, x2, 0.5, 0.5, 0.1, 0.1, 0.0) +
+#                mNd2(x1, x2, 1.0, 1.0, 0.1, 0.1, 0.0)
+#            ))
+#    
+#    return(y)
+#  }
+#  biclaw1 <-
+#    genoud(
+#      biclaw,
+#      default.domains = 20,
+#      nvars = 2,
+#      pop.size = 5000,
+#      max = TRUE,
+#      print.level = 0
+#    )
+#  expect_equal(biclaw1$value, 1.65353, tolerance=1e-6)
+#  expect_equal(biclaw1$par, c(-0.5001947, -0.5001947), tolerance=1e-6)
+#  expect_equal(biclaw1$gradients, c(-1.526799e-06, -8.571643e-07), 
+#               tolerance=1e-6)
   
 })
